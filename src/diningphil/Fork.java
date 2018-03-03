32976f0e84b8e7223f51b1ac314cbfbca4b56d92
drumsticks210 <35385985+drumsticks210@users.noreply.github.com>
2018-03-02 23:56:21 -0600

    First commit

---
+++ src/diningphil/Fork.java
@@ -0,0 +1,27 @@
+package diningphil;
+
+/**
+ *
+ * @author drums
+ */
+public class Fork {
+	private boolean isAvailable = true;
+	private int id;
+
+	public Fork(int id) {
+		super();
+		this.id = id;
+	}
+
+	public synchronized boolean isAvailable() {
+		return isAvailable;
+	}
+
+	public synchronized void setAvailable(boolean isAvailable) {
+		this.isAvailable = isAvailable;
+	}
+	
+	public String toString(){
+		return id + "";
+	}
+}
